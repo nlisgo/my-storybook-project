0a5e3c2381bc9b9b5f5457dbf3ddf1c090278555
Dave <discodavey@gmail.com>
2025-03-13 15:08:44 +0000

    Bring in json file

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,29 +7,22 @@ import Food from './components/molecules/Food'
 function App() {
   const [count, setCount] = useState(0)
 
+  const [foodData, setFoodData] = useState<{ burger: string, people: {name: string, surname: string}[] }>({
+    burger: '',
+    people: []
+  })
+
+  useEffect(() => {
+    // Fetch the food.json data
+    fetch('/src/food.json')
+      .then(response => response.json())
+      .then(data => setFoodData(data))
+      .catch(error => console.error('Error fetching food data:', error))
+  }, [])
+
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <Food burger="hotdog" people={[{name: "fred", surname: "Person"}]}/>
+      <Food burger={foodData.burger} people={foodData.people}/>
     </>
   )
 }
